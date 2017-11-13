f9c7bbcba8803833ddcfa55397b727ee7867a123
autodocs <autodocs>
2017-11-13 15:05:41 +0000

    build based on 0cbecbf

--- latest/search_index.js
+++ latest/search_index.js
@@ -53,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Library outline",
     "category": "section",
-    "text": "Pages = [\n    \"lib/interface.md\",\n]\nDepth = 2"
+    "text": "Pages = [\n    \"lib/interface.md\",\n    \"lib/discretize.md\"\n]\nDepth = 2"
 },
 
 {
@@ -86,6 +86,70 @@ var documenterSearchIndex = {"docs": [
     "title": "Posing and solving a reachability problem",
     "category": "section",
     "text": "solve"
+},
+
+{
+    "location": "lib/discretize.html#",
+    "page": "Discretization",
+    "title": "Discretization",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "lib/discretize.html#Reachability.ReachSets.discretize",
+    "page": "Discretization",
+    "title": "Reachability.ReachSets.discretize",
+    "category": "Function",
+    "text": "discretize(cont_sys, δ; [approx_model], [pade_expm], [lazy_expm])\n\nDiscretize a continuous system of ODEs with nondeterministic inputs.\n\nInput\n\ncont_sys          – continuous system\nδ                 – step size\napprox_model      – the method to compute the approximation model for the                        discretization, among:\nforward    – use forward-time interpolation\nbackward   – use backward-time interpolation\nfirstorder – use first order approximation of the ODE\nnobloating – do not bloat the initial states                 (use for discrete-time reachability)\npade_expm         – (optional, default = false) if true, use Pade approximant                        method to compute matrix exponentials of sparse matrices;                        otherwise use Julia's buil-in expm\nlazy_expm         – (optional, default = false) if true, compute the matrix                        exponential in a lazy way (suitable for very large systems)\n\nOutput\n\nA discrete system.\n\nNotes\n\nThis function applies an approximation model to transform a continuous affine system into a discrete affine system. This transformation allows to do dense time reachability, i.e. such that the trajectories of the given continuous system are included in the computed flowpipe of the discretized system. For discrete-time reachability, use approx_model=\"nobloating\".\n\n\n\n"
+},
+
+{
+    "location": "lib/discretize.html#Discretize-1",
+    "page": "Discretization",
+    "title": "Discretize",
+    "category": "section",
+    "text": "Pages = [\"discretize.md\"]\nDepth = 3CurrentModule = Reachability.ReachSetsdiscretize"
+},
+
+{
+    "location": "lib/discretize.html#Reachability.ReachSets.discr_bloat_firstorder",
+    "page": "Discretization",
+    "title": "Reachability.ReachSets.discr_bloat_firstorder",
+    "category": "Function",
+    "text": "bloat_firstorder(cont_sys, δ)\n\nCompute bloating factors using first order approximation.\n\nInput\n\ncont_sys – a continuous affine system\nδ        – step size\n\nNotes\n\nIn this algorithm, the infinity norm is used. See also: discr_bloat_interpolation for more accurate (less conservative) bounds.\n\nAlgorithm\n\nThis uses a first order approximation of the ODE, and matrix norm upper bounds, see Le Guernic, C., & Girard, A., 2010, Reachability analysis of linear systems using support functions. Nonlinear Analysis: Hybrid Systems, 4(2), 250-262.\n\n\n\n"
+},
+
+{
+    "location": "lib/discretize.html#Reachability.ReachSets.discr_bloat_interpolation",
+    "page": "Discretization",
+    "title": "Reachability.ReachSets.discr_bloat_interpolation",
+    "category": "Function",
+    "text": "discr_bloat_interpolation(cont_sys, δ, approx_model, pade_expm, lazy_expm)\n\nCompute bloating factors using forward or backward interpolation.\n\nInput\n\ncs        – a continuous system\nδ         – step size\napprox_model\npade_expm – if true, use Pade approximant method to compute the                matrix exponential\nlazy_expm – if true, compute the matrix exponential in a lazy way                suitable for very large systems)\n\nAlgorithm\n\nSee Frehse et al CAV'11 paper, SpaceEx: Scalable Verification of Hybrid Systems, see Lemma 3.\n\nNote that in the unlikely case that A is invertible, the result can also be obtained directly, as a function of the inverse of A and e^{At} - I.\n\nThe matrix P is such that: ϕAabs = P[1:n, 1:n], Phi1Aabsdelta = P[1:n, (n+1):2*n], and Phi2Aabs = P[1:n, (2*n+1):3*n].\n\n\n\n"
+},
+
+{
+    "location": "lib/discretize.html#Dense-time-reachability-1",
+    "page": "Discretization",
+    "title": "Dense-time reachability",
+    "category": "section",
+    "text": "discr_bloat_firstorder\ndiscr_bloat_interpolation"
+},
+
+{
+    "location": "lib/discretize.html#Reachability.ReachSets.discr_no_bloat",
+    "page": "Discretization",
+    "title": "Reachability.ReachSets.discr_no_bloat",
+    "category": "Function",
+    "text": "discr_no_bloat(cont_sys, δ, pade_expm, lazy_expm)\n\nDiscretize a continuous system without bloating of the initial states, suitable for discrete-time reachability.\n\nInput\n\ncont_sys     – a continuous system\nδ            – step size\npade_expm    – if true, use Pade approximant method to compute the                   matrix exponential\nlazy_expm    – if true, compute the matrix exponential in a lazy way                   (suitable for very large systems)\n\nOutput\n\nA discrete system.\n\nAlgorithm\n\nThe transformation implemented here is the following:\n\nA -> Phi := exp(A*delta)\nU -> V := M*U\nX0 -> X0hat := X0\n\nwhere M corresponds to Phi1(A, delta) in Eq. (8) of SpaceEx: Scalable Verification of Hybrid Systems.\n\nIn particular, there is no bloating, i.e. we don't bloat the initial states and dont multiply the input by the step size δ, as required for the dense time case.\n\n\n\n"
+},
+
+{
+    "location": "lib/discretize.html#Discrete-time-reachability-1",
+    "page": "Discretization",
+    "title": "Discrete-time reachability",
+    "category": "section",
+    "text": "discr_no_bloat"
 },
 
 {
