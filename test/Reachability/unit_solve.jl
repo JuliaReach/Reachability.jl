4e8fe1b9023e0d2cc936061c59b53b2eefd76f02
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2018-04-15 11:18:25 -0300

    update test

--- test/Reachability/unit_solve.jl
+++ test/Reachability/unit_solve.jl
@@ -54,5 +54,5 @@ s = solve(ContinuousSystem(sparse(A), X0), :T=>0.1, :partition=>[[i] for i in 1:
 
 A = randn(5, 5); X0 = BallInf(ones(5), 0.1)
 system = ContinuousSystem(A, X0)
-options = Options(Dict(:T=>0.1, :partition=>AbstractVector{<:AbstractVector{Int}}[1:2, 3:4, [5]], :block_types=>Dict(HPolygon=>[1:2, 3:4], Interval=>[[5]]), :vars=>[1,3]))
+options = Options(Dict(:T=>0.1, :partition=>convert(AbstractVector{<:AbstractVector{Int}}, [1:2, 3:4, [5]]), :block_types=>Dict(HPolygon=>[1:2, 3:4], Interval=>[[5]]), :vars=>[1,3]))
 s = solve(system, options)
