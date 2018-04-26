2d8e0c349800d2f7b39c6634a576f138f80ebb1b
schillic <schillic@informatik.uni-freiburg.de>
2018-04-26 14:45:46 +0200

    do not modify input options

--- src/options.jl
+++ src/options.jl
@@ -611,13 +611,14 @@ This function has several purposes:
 
 INPUT:
 
-- `dict` -- a dictionary of options
-- `dict_copy` -- a copy of the dictionary of options for internal names
-- `aliases` -- option aliases; the first name is the one we use internally
+- `dict`          -- a dictionary of options
+- `dict_copy`     -- a copy of the dictionary of options for internal names
+- `aliases`       -- option aliases; the first name is the one we use internally
 - `default_value` -- the default value for the option
-- `modify_dict` -- (optional, default true) indicates if `dict` should be modified
+- `modify_dict`   -- (optional, default: `false`) indicates if `dict` should be
+                     modified
 """
-function check_aliases_and_add_default_value!(dict::Dict{Symbol,Any}, dict_copy::Dict{Symbol,Any}, aliases::Vector{Symbol}, default_value::Any, modify_dict::Bool=true)
+function check_aliases_and_add_default_value!(dict::Dict{Symbol,Any}, dict_copy::Dict{Symbol,Any}, aliases::Vector{Symbol}, default_value::Any, modify_dict::Bool=false)
     check_aliases!(dict, dict_copy, aliases)
 
     if !haskey(dict_copy, aliases[1])
