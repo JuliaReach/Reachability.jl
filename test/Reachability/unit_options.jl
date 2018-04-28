5d8711c8bca553d72cb58983a79463c74d3a727d
schillic <schillic@informatik.uni-freiburg.de>
2018-04-28 21:47:19 +0200

    fix unit tests

--- test/Reachability/unit_options.jl
+++ test/Reachability/unit_options.jl
@@ -11,7 +11,7 @@ Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key1], 2)
 @test haskey(dict2, :key1) && dict2[:key1] == 1
 
 # use default value
-Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key2], 2)
+Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key2], 2, true)
 @test haskey(dict1, :key2) && dict1[:key2] == 2
 @test haskey(dict2, :key2) && dict2[:key2] == 2
 
@@ -30,7 +30,7 @@ Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key4, :alias4]
 @test !haskey(dict2, :alias4)
 
 # check alias detection together with default values
-Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key5, :alias5], "b")
+Reachability.check_aliases_and_add_default_value!(dict1, dict2, [:key5, :alias5], "b", true)
 @test haskey(dict1, :key5) && dict1[:key5] == "b"
 @test !haskey(dict1, :alias5)
 @test haskey(dict2, :key5) && dict2[:key5] == "b"
