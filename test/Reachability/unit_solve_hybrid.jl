26bab311fae5e3e61fd78a3bf84710f1bb4e0bfb
mforets <mforets@gmail.com>
2018-09-13 13:25:15 -0300

    Some cleanup in unit solve hybrid

--- test/Reachability/unit_solve_hybrid.jl
+++ test/Reachability/unit_solve_hybrid.jl
@@ -11,34 +11,29 @@ add_transition!(a, 1, 1, 1);
 # Modes
 A = [0.0 1.0; 0.0 0.0]
 B = reshape([0.0, -1.0], (2, 1))
-X = [HalfSpace([1.0, 0.0], 0.0)]
+X = HPolytope([HalfSpace([-1.0, 0.0], 0.0)]) # x >= 0
 U = Singleton([1.0])
 m = [ConstrainedLinearControlContinuousSystem(A, eye(size(B, 1)), X, B*U)];
 
 # Reset maps
 A = [1.0 0.0; 0.0 -0.75]
-B = zeros(2, 0)
-
-X = [HalfSpace([0.0, 1.0], 0.0),
-           HalfSpace([-1.0, 0.0], 0.0),# x = 0
-           HalfSpace([1.0, 0.0], 0.0)] # v < 0
-U = Vector{LazySet{Float64}}()
-r = [ConstrainedLinearControlDiscreteSystem(A, B, X, U)];
+X = HPolytope([HalfSpace([0.0, 1.0], 0.0),  # v <= 0
+               HalfSpace([-1.0, 0.0], 0.0), # x >= 0
+               HalfSpace([1.0, 0.0], 0.0)]) # x <= 0
+r = [ConstrainedLinearDiscreteSystem(A, X)];
 
 # Switchings
 s = [HybridSystems.AutonomousSwitching()];
 
-H = HybridSystem(a, m, r, s)
+HS = HybridSystem(a, m, r, s)
 
 # initial condition in mode 1
 X0 = BallInf(zeros(2), 0.01)
 
 # calculate reachable states up to time T
-prob = InitialValueProblem(H, X0)
+prob = InitialValueProblem(HS, X0)
 input_options = Options(:mode=>"reach")
 
-
-problem_options = Options(:vars=>[1,2], :T=>1.0,
-                    :δ=>0.01);
+problem_options = Options(:vars=>[1,2], :T=>1.0, :δ=>0.01);
 options = merge(problem_options, input_options)
-sol = solve_hybrid(H, X0, options)
+sol = solve_hybrid(HS, X0, options)
