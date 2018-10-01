674f23c5acb39b9de3d013e6d38c7d710950a49f
schillic <schillic@informatik.uni-freiburg.de>
2018-10-01 15:52:33 +0200

    add assertion

--- src/solve.jl
+++ src/solve.jl
@@ -351,6 +351,9 @@ with Support Functions](http://spaceex.imag.fr/sites/default/files/frehser_adhs2
 function solve_hybrid(HS::HybridSystem,
                       X0::LazySet,
                       options::Options)::AbstractSolution
+    @assert isdefined(Main, :Polyhedra) "this algorithm needs the package " *
+            "'Polyhedra' to be loaded"
+
     # set options
     options.dict[:project_reachset] = false
 
