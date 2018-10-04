3bbfdd6b9823bc696d60587704b63d5b18ac314d
schillic <schillic@informatik.uni-freiburg.de>
2018-10-04 21:04:04 +0200

    minor refactoring hybrid tests

--- test/Reachability/unit_solve_hybrid.jl
+++ test/Reachability/unit_solve_hybrid.jl
@@ -1,2 +1,2 @@
-@time @testset "Reachability.solve" begin include("unit_solve_hybrid_bouncing_ball.jl") end
-@time @testset "Reachability.solve" begin include("unit_solve_hybrid_thermostat.jl") end
+include("unit_solve_hybrid_bouncing_ball.jl")
+include("unit_solve_hybrid_thermostat.jl")
