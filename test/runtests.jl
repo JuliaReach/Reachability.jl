fc0be03299433140bb5ea321e419d3af9fcf0fc9
mforets <mforets@gmail.com>
2018-12-13 12:27:36 -0300

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,9 @@ import LazySets.LinearMap
 include("../src/compat.jl")
 using Compat.Test
 
+# Note: CDDLib is only required for v0.6; for v0.7 or later the default Polyhedra
+# library is used and CDDLib can be removed from REQUIRE
+
 include("Systems/alltests.jl")
 include("ReachSets/alltests.jl")
 include("Reachability/alltests.jl")
