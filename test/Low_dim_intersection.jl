79764a087514aa847f6d2450dbfe8659e5737782
Unknown <kostemkin@gmail.com>
2019-01-23 17:53:46 +1100

    minor refactor

--- test/Low_dim_intersection.jl
+++ test/Low_dim_intersection.jl
@@ -1,4 +1,4 @@
-using LazySets
+using LazySets, Optim
 import LazySets.Approximations:decompose, project, overapproximate
 
 n0 = 16
@@ -9,9 +9,9 @@ X0 = Hyperrectangle(low=[0.;0.],
 
 proj_X0 = decompose(X0, blocks=[1, dim(X0)-1])
 G = HPolyhedron([HalfSpace([1.0; 0.], 0.5)])
-proj_G = project(G, [1])
+proj_G = project(G, [1], LinearMap)
 
 proj_inter = proj_G ∩ proj_X0.array[1]
 proj_X0.array[1] = overapproximate(proj_inter, Hyperrectangle)
 
-plot(project(proj_X0,[1,2]))
+plot(project(proj_X0,[1,2], Hyperrectangle))
