0e67a508864340246461c7739c665cb80257d045
schillic <schillic@informatik.uni-freiburg.de>
2019-02-09 21:09:23 +0100

    update hybrid models in tests

--- test/Reachability/alltests.jl
+++ test/Reachability/alltests.jl
@@ -1,4 +1,3 @@
 @time @testset "Reachability.options" begin include("unit_options.jl") end
 @time @testset "Reachability.solve_continuous" begin include("solve_continuous.jl") end
-@time @testset "Reachability.solve_hybrid_bouncing_ball" begin include("solve_hybrid_bouncing_ball.jl") end
-@time @testset "Reachability.solve_hybrid_thermostat" begin include("solve_hybrid_thermostat.jl") end
+@time @testset "Reachability.solve_hybrid" begin include("solve_hybrid.jl") end
