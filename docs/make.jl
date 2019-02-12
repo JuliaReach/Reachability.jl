65ea4df04616eac622a9b91d17f693796e2bba90
schillic <schillic@informatik.uni-freiburg.de>
2019-02-12 07:58:16 +0100

    correct build version for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -25,7 +25,7 @@ deploydocs(
     repo = "github.com/JuliaReach/Reachability.jl.git",
     target = "build",
     osname = "linux",
-    julia  = "0.6",
+    julia  = "1.0",
     deps = nothing,
     make = nothing
 )
