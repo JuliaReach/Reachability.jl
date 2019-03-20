bbd8ee5af0fc356df6321240c4dc3fb6ab9c9164
mforets <mforets@gmail.com>
2019-03-20 08:40:04 -0300

    fix projection

--- src/ReachSets/ContinuousPost/GLGM06/GLGM06.jl
+++ src/ReachSets/ContinuousPost/GLGM06/GLGM06.jl
@@ -29,12 +29,13 @@ import LazySets.Approximations: project
 # each time lapse
 function add_time(sol::ReachSolution{Zonotope})
     N = length(sol.Xk)
-    sol_with_time = Vector{ReachSet{Zonotope}}(undef, N)
+    sol_with_time = Vector{ReachSet{Zonotope{Float64}, Float64}}(undef, N)
     @inbounds for i in eachindex(sol.Xk)
         t0, t1 = sol.Xk[i].t_start, sol.Xk[i].t_end
         radius = (t1 - t0)/2.0
         Xk_i = sol.Xk[i].X × Zonotope([t0 + radius], hcat(radius)) # BallInf([t0 + radius], radius)
-        sol_with_time[i] = ReachSet(convert(Zonotope, Xk_i), t0, t1)
+        Xk_i = convert(Zonotope, Xk_i)
+        sol_with_time[i] = ReachSet(Xk_i, t0, t1)
     end
     options = copy(sol.options)
     options[:n] += 1 # update state space dimension
@@ -43,19 +44,21 @@ end
 
 function project(sol::ReachSolution{Zonotope})
     N = length(sol.Xk)  # number of reach sets
-    n = sol.options[:n] # state space dimension
     πsol = Vector{Zonotope}(undef, N) # preallocated projected reachsets
     πvars = sol.options[:plot_vars] # variables for plotting
+    @assert length(πvars) == 2
 
     if 0 ∈ πvars
         # add the time variable to the model (it is assumed it is not already
-        # part of the model, otherwise use the corresponding index number)
-        add_time(sol)
+        # a variable in the model)
+        sol = add_time(sol)
+        πvars[first(indexin(0, πvars))] = sol.options[:n]  # time index is added in the end
     end
+    n = sol.options[:n] # state space dimension (possibly enlarged in add_time)
 
     for i in eachindex(sol.Xk)
-        M = sparse(1:n, πvars, ones(N, m), 2, n)
-        πvars[i] = linear_map(M, sol.Xk[i].X)
+        M = sparse([1, 2], πvars, [1.0, 1.0], 2, n)
+        πsol[i] = linear_map(M, sol.Xk[i].X)
     end
     return πsol
 end
