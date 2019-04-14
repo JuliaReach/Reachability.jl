f1d1b6f442a2497cc024cb961ac66cff91bc0e34
Aadesha <aadeshdeshmukh25@gmail.com>
2019-04-15 02:26:10 +0530

    Update reach.jl

--- src/ReachSets/ContinuousPost/TMJets/reach.jl
+++ src/ReachSets/ContinuousPost/TMJets/reach.jl
@@ -15,7 +15,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.=#
 
-using TaylorSeries:get_numvars
+using TaylorSeries
 using TaylorIntegration:jetcoeffs!
 using TaylorModels: validated_step!,TaylorModelN
 import IntervalArithmetic
