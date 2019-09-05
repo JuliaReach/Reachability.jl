0a691082c630e2e2a8596c534f397582d17aa351
Marcelo Forets <mforets@gmail.com>
2019-09-05 16:02:21 -0300

    #651 Update TMJets (#657)
    * update Project.toml
    
    * update TMJets
    
    * Update Project.toml

--- src/ReachSets/ContinuousPost/TMJets/reach.jl
+++ src/ReachSets/ContinuousPost/TMJets/reach.jl
@@ -19,59 +19,59 @@ SOFTWARE.
 
 import IntervalArithmetic
 using TaylorSeries
-using TaylorIntegration: jetcoeffs!
+import TaylorIntegration
 using TaylorModels: validated_step!, TaylorModel1, TaylorModelN
 
+import IntervalArithmetic
+const IA = IntervalArithmetic
+const TI = TaylorIntegration
+
 function validated_integ(f!, qq0::AbstractVector{T}, δq0::IntervalBox{N,T},
-        t0::T, tmax::T, orderQ::Int, orderT::Int, abstol::T;
-        maxsteps::Int=500, parse_eqs::Bool=true,
-        check_property::Function=(t, x)->true) where {N, T<:Real}
+                         t0::T, tmax::T, orderQ::Int, orderT::Int, abstol::T,
+                         params=nothing; # not currently used
+                         maxsteps::Int=500, parse_eqs::Bool=true,
+                         check_property::Function=(t, x)->true) where {N, T<:Real}
 
     # Set proper parameters for jet transport
     @assert N == get_numvars()
     dof = N
 
     # Some variables
-    R = IntervalArithmetic.Interval{T}
+    zI = zero(IA.Interval{T})
     q0 = IntervalBox(qq0)
     t = t0 + Taylor1(orderT)
     tI = t0 + Taylor1(orderT+1)
-    δq_norm = IntervalBox(IntervalArithmetic.Interval{T}(-1,1), Val(N))
+    δq_norm = IntervalBox(IA.Interval{T}(-1,1), N)
     q0box = q0 .+ δq_norm
 
     # Allocation of vectors
     # Output
-    tv    = Vector{T}(undef, maxsteps+1)
-    xv    = Vector{IntervalBox{N,T}}(undef, maxsteps+1)
+    tv = Vector{T}(undef, maxsteps+1)
+    xv = Vector{IntervalBox{N,T}}(undef, maxsteps+1)
     xTM1v = Matrix{TaylorModel1{TaylorN{T}, T}}(undef, dof, maxsteps+1)
 
     # Internals: jet transport integration
-    x     = Vector{Taylor1{TaylorN{T}}}(undef, dof)
-    dx    = Vector{Taylor1{TaylorN{T}}}(undef, dof)
-    xaux  = Vector{Taylor1{TaylorN{T}}}(undef, dof)
-    x0    = Vector{TaylorN{T}}(undef, dof)
-    xTMN  = Vector{TaylorModelN{N,T,T}}(undef, dof)
+    x = Vector{Taylor1{TaylorN{T}}}(undef, dof)
+    dx = Vector{Taylor1{TaylorN{T}}}(undef, dof)
+    xaux = Vector{Taylor1{TaylorN{T}}}(undef, dof)
+    xTMN = Vector{TaylorModelN{N, T, T}}(undef, dof)
 
     # Internals: Taylor1{Interval{T}} integration
-    xI    = Vector{Taylor1{IntervalArithmetic.Interval{T}}}(undef, dof)
-    dxI   = Vector{Taylor1{IntervalArithmetic.Interval{T}}}(undef, dof)
-    xauxI = Vector{Taylor1{IntervalArithmetic.Interval{T}}}(undef, dof)
-    x0I   = Vector{IntervalArithmetic.Interval{T}}(undef, dof)
+    xI = Vector{Taylor1{IA.Interval{T}}}(undef, dof)
+    dxI = Vector{Taylor1{IA.Interval{T}}}(undef, dof)
+    xauxI = Vector{Taylor1{IA.Interval{T}}}(undef, dof)
 
     # Set initial conditions
-    zI = zero(R)
-    rem = Vector{IntervalArithmetic.Interval{T}}(undef, dof)
+    rem = Vector{IA.Interval{T}}(undef, dof)
 
     @inbounds for i in eachindex(x)
         qaux = normalize_taylor(qq0[i] + TaylorN(i, order=orderQ), δq0, true)
         x[i] = Taylor1(qaux, orderT)
         dx[i] = x[i]
-        x0[i] = copy(qaux)
-        xTMN[i] = TaylorModelN(x[i][0], zI, q0, q0box)
+        xTMN[i] = TaylorModelN(qaux, zI, q0, q0box)
 
-        xI[i] = Taylor1(q0box[i], orderT+1)
+        xI[i] = Taylor1(q0[i] + δq0[i], orderT+1)
         dxI[i] = xI[i]
-        x0I[i] = qaux(δq_norm)
         rem[i] = zI
 
         xTM1v[i, 1] = TaylorModel1(deepcopy(x[i]), zI, zI, zI)
@@ -79,13 +79,13 @@ function validated_integ(f!, qq0::AbstractVector{T}, δq0::IntervalBox{N,T},
 
     # Output vectors
     @inbounds tv[1] = t0
-    @inbounds xv[1] = IntervalBox(evaluate(xTMN, δq_norm))
+    @inbounds xv[1] = evaluate(xTMN, δq_norm)
 
     # Determine if specialized jetcoeffs! method exists (built by @taylorize)
-    parse_eqs = parse_eqs && (length(methods(jetcoeffs!)) > 2)
+    parse_eqs = parse_eqs && (length(methods(TI.jetcoeffs!)) > 2)
     if parse_eqs
         try
-            jetcoeffs!(Val(f!), t, x, dx)
+            TI.jetcoeffs!(Val(f!), t, x, dx, params)
         catch
             parse_eqs = false
         end
@@ -97,8 +97,8 @@ function validated_integ(f!, qq0::AbstractVector{T}, δq0::IntervalBox{N,T},
 
         # Validated step of the integration
         δt = validated_step!(f!, t, x, dx, xaux, tI, xI, dxI, xauxI,
-            t0, tmax, x0, x0I, xTMN, xv, rem, δq_norm,
-            q0, q0box, nsteps, orderT, abstol, parse_eqs, check_property)
+            t0, tmax, xTMN, xv, rem, δq_norm, q0, q0box,
+            nsteps, orderT, abstol, params, parse_eqs, check_property)
 
         # New initial conditions and time
         nsteps += 1
@@ -108,12 +108,14 @@ function validated_integ(f!, qq0::AbstractVector{T}, δq0::IntervalBox{N,T},
             tI[0] = t0
             tv[nsteps] = t0
             for i in eachindex(x)
-                xTM1v[i, nsteps] = TaylorModel1(deepcopy(x[i]), rem[i],
-                                        IntervalArithmetic.Interval(0, 0),
-                                        IntervalArithmetic.Interval(0, δt))
+                δtI = IA.Interval(0, δt)
+                xTM1v[i, nsteps] = TaylorModel1(deepcopy(x[i]), rem[i], zI, δtI)
                 aux = x[i](δt)
                 x[i]  = Taylor1(aux, orderT)
                 dx[i] = Taylor1(zero(aux), orderT)
+                auxI = xTMN[i](δq_norm)
+                xI[i] = Taylor1(auxI, orderT+1)
+                dxI[i] = xI[i]
             end
         end
 
@@ -124,7 +126,5 @@ function validated_integ(f!, qq0::AbstractVector{T}, δq0::IntervalBox{N,T},
 
     end
 
-    return view(tv,1:nsteps), view(xv,1:nsteps), view(xTM1v, :, 1:nsteps)
+    return view(tv, 1:nsteps), view(xv, 1:nsteps), view(xTM1v, :, 1:nsteps)
 end
-
-
