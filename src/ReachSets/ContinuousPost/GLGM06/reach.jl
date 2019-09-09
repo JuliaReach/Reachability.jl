afbb15acbb9729b32df0d06e71cd0d6d49e02b26
schillic <schillic@informatik.uni-freiburg.de>
2019-09-09 15:18:22 +0200

    polishing of GLGM06

--- src/ReachSets/ContinuousPost/GLGM06/reach.jl
+++ src/ReachSets/ContinuousPost/GLGM06/reach.jl
@@ -1,66 +1,64 @@
-# ===============================================================
+# ================
 # Homogeneous case
-# ===============================================================
-function reach_homog!(HR::Vector{ReachSet{Zonotope{Float64}}},
+# ================
+
+function reach_homog!(R::Vector{ReachSet{Zonotope{Float64}}},
                       Ω0::Zonotope,
                       Φ::AbstractMatrix,
                       N::Int,
                       δ::Float64,
                       max_order::Int)
-
-    # save timestamps with the reach set
-    t0, t1 = zero(δ), δ
-
     # initial reach set
-    HR[1] = ReachSet(Ω0, t0, t1)
+    t0, t1 = zero(δ), δ
+    R[1] = ReachSet(Ω0, t0, t1)
 
     k = 2
     while k <= N
-        HR_next = linear_map(Φ, HR[k-1].X)
-        HR_next_red = reduce_order(HR_next, max_order)
-        t0 = t1; t1 += δ
-        HR[k] = ReachSet(HR_next_red, t0, t1)
+        Rₖ = linear_map(Φ, R[k-1].X)
+        Rₖ = reduce_order(Rₖ, max_order)
+        t0 = t1
+        t1 += δ
+        R[k] = ReachSet(Rₖ, t0, t1)
+
         k += 1
     end
-    return HR
+    return R
 end
 
-# ===============================================================
+# ==================
 # Inhomogeneous case
-# ===============================================================
-function reach_inhomog!(X::Vector{ReachSet{Zonotope{Float64}}},
+# ==================
+
+function reach_inhomog!(R::Vector{ReachSet{Zonotope{Float64}}},
                         Ω0::Zonotope,
                         U::ConstantInput,
                         Φ::AbstractMatrix,
                         N::Int,
                         δ::Float64,
                         max_order::Int)
-
-    # initialization
+    # initial reach set
     t0, t1 = zero(δ), δ
-    V = next_set(U)
+    R[1] = ReachSet(Ω0, t0, t1)
 
-    X[1] = ReachSet(Ω0, t0, t1)
+    V = next_set(U)
     Wk₊ = V
     Φ_power_k = copy(Φ)
     Φ_power_k_cache = similar(Φ)
 
-    # update
     k = 2
     while k <= N
-        Xk = minkowski_sum(linear_map(Φ_power_k, Ω0), Wk₊)
+        Rₖ = minkowski_sum(linear_map(Φ_power_k, Ω0), Wk₊)
+        Rₖ = reduce_order(Rₖ, max_order)
+        t0 = t1
+        t1 += δ
+        R[k] = ReachSet(Rₖ, t0, t1)
+
         Wk₊ = minkowski_sum(Wk₊, linear_map(Φ_power_k, V))
-
-        t0 = t1; t1 += δ
-        Xk_red = reduce_order(Xk, max_order)
-        X[k] = ReachSet(Xk_red, t0, t1)
-
         Wk₊ = reduce_order(Wk₊, max_order)
 
         mul!(Φ_power_k_cache, Φ_power_k, Φ)
         copyto!(Φ_power_k, Φ_power_k_cache)
         k += 1
     end
-
-    return X 
+    return R
 end
