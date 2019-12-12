490c4a5f4dedb25b3919f10f396c62aebb56febb
schillic <schillic@informatik.uni-freiburg.de>
2019-12-12 11:10:38 +0100

    rewrite project helpers

--- src/ReachSets/ContinuousPost/GLGM06/project.jl
+++ src/ReachSets/ContinuousPost/GLGM06/project.jl
@@ -1,6 +1,6 @@
-# =======================================
-# Functionality to project the flowpipe
-# =======================================
+# ======================================
+# Functionality to project the flowpipe
+# ======================================
 import LazySets.Approximations: project
 
 # add a "time" variable by taking the cartesian product of the flowpipe ℱ with each time lapse
@@ -17,32 +17,35 @@ function add_time(ℱ::Vector{ReachSet{Zonotope{Float64}}})
 end
 
 function project(sol::ReachSolution{Zonotope{Float64}})
-    N = length(sol.Xk)  # number of reach sets
-    n = dim(set(first(sol.Xk))) # state space dimension
+    n = dim(set(first(sol.flowpipes[1].reachsets))) # state space dimension
     options = copy(sol.options)
-    πℱ = Vector{ReachSet{Zonotope{Float64}}}(undef, N) # preallocated projected reachsets
     πvars = sol.options[:plot_vars] # variables for plotting
     @assert length(πvars) == 2
 
-    if 0 ∈ πvars
-        # add the time variable to the flowpipe (assuming it's not already
-        # part of the model)
-        ℱ = add_time(sol.Xk)
-        n += 1
-        options[:n] += 1 # TODO : remove when option is removed
-        πvars = copy(πvars)
-        πvars[first(indexin(0, πvars))] = n # time index is added in the end
-    else
-        ℱ = sol.Xk
-    end
+    proj_flowpipes = Vector{Flowpipe}(undef, length(sol.flowpipes))
+    for (j, flowpipe) in enumerate(sol.flowpipes)
+        if 0 ∈ πvars
+            # add the time variable to the flowpipe (assuming it's not already
+            # part of the model)
+            ℱ = add_time(flowpipe.reachsets)
+            n += 1
+            options[:n] += 1 # TODO : remove when option is removed
+            πvars = copy(πvars)
+            πvars[first(indexin(0, πvars))] = n # time index is added in the end
+        else
+            ℱ = flowpipe.reachsets
+        end
 
-    M = sparse([1, 2], πvars, [1.0, 1.0], 2, n)
-    for i in eachindex(ℱ)
-        t0, t1 = time_start(ℱ[i]), time_end(ℱ[i])
-        πℱ_i = linear_map(M, set(ℱ[i]))
-        πℱ_i = Zonotope(πℱ_i.center, πℱ_i.generators)
-        πℱ_i = reduce_order(πℱ_i, options[:max_order])
-        πℱ[i] = ReachSet{Zonotope{Float64}}(πℱ_i, t0, t1)
+        πℱ = Vector{ReachSet{Zonotope{Float64}}}(undef, length(flowpipe.reach_sets))
+        M = sparse([1, 2], πvars, [1.0, 1.0], 2, n)
+        for i in eachindex(ℱ)
+            t0, t1 = time_start(ℱ[i]), time_end(ℱ[i])
+            πℱ_i = linear_map(M, set(ℱ[i]))
+            πℱ_i = Zonotope(πℱ_i.center, πℱ_i.generators)
+            πℱ_i = reduce_order(πℱ_i, options[:max_order])
+            πℱ[i] = ReachSet{Zonotope{Float64}}(πℱ_i, t0, t1)
+        end
+        proj_flowpipes[j] = πℱ
     end
-    return ReachSolution(πℱ, options)
+    return ReachSolution(proj_flowpipes, options)
 end
--- src/ReachSets/ContinuousPost/TMJets/project.jl
+++ src/ReachSets/ContinuousPost/TMJets/project.jl
@@ -1,6 +1,6 @@
-# =======================================
-# Functionality to project the flowpipe
-# =======================================
+# ======================================
+# Functionality to project the flowpipe
+# ======================================
 import LazySets.Approximations: project
 using LazySets.Approximations: overapproximate
 
@@ -19,32 +19,34 @@ function add_time(ℱ::Vector{ReachSet{Hyperrectangle{T, VC, VR}}}) where {T, VC
 end
 
 function project(sol::ReachSolution{Hyperrectangle{T, VC, VR}}) where {T, VC, VR}
-    N = length(sol.Xk)  # number of reach sets
-    n = dim(set(first(sol.Xk))) # state space dimension
-
+    n = dim(set(first(sol.flowpipes[1].reachsets))) # state space dimension
     options = copy(sol.options)
     πvars = sol.options[:plot_vars] # variables for plotting
     @assert length(πvars) == 2
 
-    if 0 ∈ πvars # if we want to project along time
-        ℱ = add_time(sol.Xk)
-        n += 1
-        options[:n] += 1 # TODO : remove when option is removed
-        πvars = copy(πvars)
-        πvars[first(indexin(0, πvars))] = n # time index is added in the end
-    else
-        ℱ = sol.Xk
-    end
+    proj_flowpipes = Vector{Flowpipe}(undef, length(sol.flowpipes))
+    for (j, flowpipe) in enumerate(sol.flowpipes)
+        if 0 ∈ πvars # if we want to project along time
+            ℱ = add_time(flowpipe.reachsets)
+            n += 1
+            options[:n] += 1 # TODO : remove when option is removed
+            πvars = copy(πvars)
+            πvars[first(indexin(0, πvars))] = n # time index is added in the end
+        else
+            ℱ = flowpipe.reachsets
+        end
 
-    HT = Hyperrectangle{T, Vector{T}, Vector{T}} # we hardcode dense vectors here,
-    # as using VC and VR would require to adapt overapproximate
-    πℱ = Vector{ReachSet{HT}}(undef, N) # preallocate projected reachsets
+        HT = Hyperrectangle{T, Vector{T}, Vector{T}} # we hardcode dense vectors here,
+        # as using VC and VR would require to adapt overapproximate
+        πℱ = Vector{ReachSet{HT}}(undef, length(flowpipe.reach_sets))
 
-    M = sparse([1, 2], πvars, [1.0, 1.0], 2, n)
-    for i in eachindex(ℱ)
-        t0, t1 = time_start(ℱ[i]), time_end(ℱ[i])
-        πℱ_i = overapproximate(M * set(ℱ[i]), Hyperrectangle)
-        πℱ[i] = ReachSet(πℱ_i, t0, t1)
+        M = sparse([1, 2], πvars, [1.0, 1.0], 2, n)
+        for i in eachindex(ℱ)
+            t0, t1 = time_start(ℱ[i]), time_end(ℱ[i])
+            πℱ_i = overapproximate(M * set(ℱ[i]), Hyperrectangle)
+            πℱ[i] = ReachSet(πℱ_i, t0, t1)
+        end
+        proj_flowpipes[j] = πℱ
     end
-    return ReachSolution(πℱ, options)
+    return ReachSolution(proj_flowpipes, options)
 end
